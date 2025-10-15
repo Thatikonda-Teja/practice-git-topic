b0b8fe838717a46e7e1e0f6718559f90f3f699b8
Teja.Thatikonda <tejathatikonda555@gmail.com>
2025-10-15 13:01:04 +0530

    add demo component in  project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Demo from './demo';
 
 function App() {
   return (
     <div className="App">
-      <h1>hi react </h1>
+      {/* <h1>hi react </h1> */}
+     <Demo/>
     </div>
   );
 }
---
+++ src/demo.js
@@ -0,0 +1,7 @@
+export default function Demo(){
+    return(
+        <div>
+            <h1>this is demo component</h1>
+        </div>
+    )
+}
